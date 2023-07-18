5663707b022ec6e659bc69073b3c30eee50bcdd5
Ali Yar Khan <aliyark145@gmail.com>
2023-07-18 23:30:12 +0500

    first commit with sequelize models, migrations and seeds

---
+++ seeders/20230717154838-demo-user.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    return queryInterface.bulkInsert("User", [
+      {
+        firstName: "Ali Yar",
+        lastName: "Khan",
+        email: "aliyark145@gmail.com",
+        phoneNumber: "+923347724898",
+        password: "541krayilA@",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    return queryInterface.bulkDelete("User", null, {});
+  },
+};
