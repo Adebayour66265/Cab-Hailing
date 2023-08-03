2035ce4ce9d50b7b41ab3e7a632989e4b02c0944
Ali Yar Khan <aliyark145@gmail.com>
2023-08-03 22:16:28 +0500

    request ride and fetch all nearby rides

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const RequestRide = require("./models/requestRide");
 const connectDB = require("./config/db");
 const http = require("http");
 const server = http.createServer(app);
@@ -11,30 +12,80 @@ const io = new Server(server);
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
-app.use(cors(
-    {
-        origin: '*',
-        methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
-        allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'x-access-token', 'X-Requested-With', 'Accept', 'Access-Control-Allow-Headers', 'Access-Control-Request-Headers', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Methods', 'Access-Control-Allow-Credentials'],
-    }
-));
-app.use(bodyParser.urlencoded({ extended: true}));
-
-connectDB()
-app.get('/', (req, res) => {
-    res.send('Hello World');
-})
-app.use('/', require('./routes/user'))
-app.use('/vehicle', require('./routes/owner'))
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Origin",
+      "x-access-token",
+      "X-Requested-With",
+      "Accept",
+      "Access-Control-Allow-Headers",
+      "Access-Control-Request-Headers",
+      "Access-Control-Allow-Origin",
+      "Access-Control-Allow-Methods",
+      "Access-Control-Allow-Credentials",
+    ],
+  })
+);
+app.use(bodyParser.urlencoded({ extended: true }));
 
 connectDB();
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 app.use("/", require("./routes/user"));
-io.on("connection", (socket) => {
-  console.log("a user connected");
+app.use("/vehicle", require("./routes/owner"));
+
+connectDB();
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
+app.use("/", require("./routes/user"));
+
+io.on("connection", (socket) => {
+  socket.on("ride request", async (args) => {
+    try {
+      const rr = new RequestRide({
+        userId: args.userId,
+        vehicleId: args.vehicleId,
+        requestType: args.requestType,
+        status: args.status,
+        pickupLocation: { type: "Point", coordinates: args.pickupLocation },
+        dropoffLocation: { type: "Point", coordinates: args.dropoffLocation },
+        paymentMethod: args.paymentMethod,
+        totalCost: args.totalCost,
+        additionalFees: args.additionalFees,
+      });
+      await rr.save();
+      socket.emit("ride request success", { success: true });
+    } catch (error) {
+      socket.emit("ride request failed", { error: error.message });
+    }
+  });
+  socket.on("get ride requests", async (args) => {
+    try {
+      const nearbyRequests = await RequestRide.find({
+        pickupLocation: {
+          $near: {
+            $geometry: {
+              type: "Point",
+              coordinates: [args.longitude, args.latitude],
+            },
+            $maxDistance: args.maxDistance,
+          },
+        },
+      });
+      socket.emit("nearbyRideRequests", { requests: nearbyRequests });
+    } catch (error) {
+      socket.emit("nearbyRideRequestsError", { error: error.message });
+    }
+  });
+});
+
 app.listen(process.env.PORT, () => {
   console.log("Server is running on port " + process.env.PORT);
 });
---
+++ models/requestRide.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const requestRide = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  vehicleId: {
+    type: Schema.Types.ObjectId,
+    ref: "Vehicle",
+    required: true,
+  },
+  requestType: {
+    type: String,
+    enum: ["car", "tricycle"],
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["pending", "active", "completed", "cancelled"],
+    default: "pending",
+  },
+  pickupLocation: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+      index: "2dsphere",
+    },
+  },
+  dropoffLocation: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+      index: "2dsphere",
+    },
+  },
+  paymentMethod: {
+    type: String,
+    required: true,
+  },
+  totalCost: {
+    type: Number,
+    required: true,
+  },
+  additionalFees: {
+    type: Number,
+    required: true,
+  },
+});
+
+const RequestRide = mongoose.model("RequestRide", requestRide);
+
+module.exports = RequestRide;
